a5a1e5a44a5bd2e19c152613c19dfd77b063925e
Neal Archival <73256760+nealarch01@users.noreply.github.com>
2023-07-27 17:35:47 -0700

    Initial Commit

---
+++ SwiftDataTodoFun/SwiftDataTodoFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTodoFunApp.swift
+//  SwiftDataTodoFun
+//
+//  Created by Neal Archival on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTodoFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
